79ed059039638527344edba165fdcfcc4ed31106
serendipity80-dev <ljiljana.bogdanovic@yahoo.com>
2021-03-29 15:01:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import Navbar from './components/Navbar';
+import Users from './components/Users';
 import './App.css';
 
 function App() {
+  const [users,setUsers] = useState([]);
+// const API_KEY = 'a14a734950ed43f096f4ad0d827aa987896e3a17';
+  useEffect(() => {
+    fetch('https://api.github.com/users')
+    .then(response =>response.json())
+    .then (data => {
+      setUsers(data) // set users in state
+    console.log(data)
+
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <Navbar/> 
+     <div className="users">
+     {users.map((user)=>{
+       return <Users key={user.id} {...user}/>
+     })}  
+     </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {FaGithub} from 'react-icons/fa';
+
+const Navbar = () => {
+    return (
+        <div className="icon">
+           <FaGithub/>userSearch 
+        </div>
+    )
+}
+
+export default Navbar
